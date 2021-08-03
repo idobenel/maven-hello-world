08d679be7ad9e02fd2e5505e832de4073ae2ddd4
idobenel <88383975+idobenel@users.noreply.github.com>
2021-08-03 16:19:35 +0300

    Add my name to the message
    Add my name to the message

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! My name is Ido!" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
